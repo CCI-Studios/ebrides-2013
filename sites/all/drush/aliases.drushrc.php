2cde9c967343038f9080edf9c42e3f938acf4998
Craig Hunt <chunt@ccistudios.com>
2013-02-04 09:23:09 -0500

    All Stuff

---
+++ sites/all/drush/aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+
+$aliases['dev'] = array(
+	'uri'=> 'ebrides.ccistaging.com',
+	'root' => '/home/staging/subdomains/ebrides/public_html',
+	'remote-host'=> 'ccistaging.com',
+	'remote-user'=> 'staging',
+	'path-aliases'=> array(
+	'%files'=> 'sites/default/files',
+	),
+);
